a79bacfa2a37ae3632b0c7d6a27f4640e179cfd9
Balti Nour <nour.balti@etudiant-fst.utm.tn>
2022-12-24 01:57:58 +0100

    end of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+        {
+        }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+  
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
